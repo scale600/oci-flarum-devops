724b0448c33b0a4d6306230b2d70e92ee668eea7
Richard Lee <riderwin.go@gmail.com>
2025-10-25 16:26:47 -0700

    Fix OCI capacity issue - switch to VM.Standard.E4.Flex
    - Change from VM.Standard.E2.1 to VM.Standard.E4.Flex
    - Add shape_config with 2 OCPUs and 12GB RAM
    - Fix 'Out of host capacity' error in US-SANJOSE-1-AD-1
    - Use current generation instance type for better availability
    - Same performance specs: 2 OCPU, 12GB RAM

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ data "oci_core_images" "oracle_linux" {
   compartment_id   = var.compartment_id
   operating_system = "Oracle Linux"
   operating_system_version = "8"
-  shape            = "VM.Standard.E2.1"
+  shape            = "VM.Standard.E4.Flex"
   sort_by          = "TIMECREATED"
   sort_order       = "DESC"
 }
@@ -169,7 +169,12 @@ resource "oci_core_instance" "flarum_instance" {
   compartment_id      = var.compartment_id
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   display_name        = "flarum-web-server"
-  shape               = "VM.Standard.E2.1"
+  shape               = "VM.Standard.E4.Flex"
+  
+  shape_config {
+    ocpus         = 2
+    memory_in_gbs = 12
+  }
 
   source_details {
     source_type = "image"
