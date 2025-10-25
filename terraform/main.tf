1de443a702a4d2bfc096dab0e064c51fe9c7b088
Richard Lee <riderwin.go@gmail.com>
2025-10-25 16:29:03 -0700

    Revert to Always Free Tier due to OCI capacity issues
    - Change back to VM.Standard.E2.1.Micro (Always Free)
    - Remove shape_config for Flex instances
    - Restore micro instance optimizations:
      - 2GB swap file for micro instance
      - Skip system package updates (too slow)
      - Install only essential packages
    - Fix persistent 'Out of host capacity' errors
    - Ensure deployment success with Always Free Tier

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ data "oci_core_images" "oracle_linux" {
   compartment_id   = var.compartment_id
   operating_system = "Oracle Linux"
   operating_system_version = "8"
-  shape            = "VM.Standard.E4.Flex"
+  shape            = "VM.Standard.E2.1.Micro"
   sort_by          = "TIMECREATED"
   sort_order       = "DESC"
 }
@@ -169,12 +169,7 @@ resource "oci_core_instance" "flarum_instance" {
   compartment_id      = var.compartment_id
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   display_name        = "flarum-web-server"
-  shape               = "VM.Standard.E4.Flex"
-  
-  shape_config {
-    ocpus         = 2
-    memory_in_gbs = 12
-  }
+  shape               = "VM.Standard.E2.1.Micro"
 
   source_details {
     source_type = "image"
